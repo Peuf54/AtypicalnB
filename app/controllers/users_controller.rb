ca5498c6f672335e79bd85d016161f7c0a3b9e20
Hugo Petament <h.petament@icloud.com>
2023-07-22 00:05:37 +0200

    Fix image display and user form for photo upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :overview)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :overview, :photo)
   end
 end
