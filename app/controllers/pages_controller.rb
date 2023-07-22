0949e27f9102a8937a18bc932a1b813154470a43
Hugo Petament <h.petament@icloud.com>
2023-07-22 09:17:21 +0200

    fix home page bug when switching renter/owner mode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     if current_user&.owner_mode
-      current_user.owner_mode = false
-      current_user.save
-      redirect_to root_path
+      redirect_to revenue_path
     end
     @properties = Property.order(rating: :desc).limit(5)
+  end
+
+  def revenue
+    if current_user && !current_user.owner_mode
+      redirect_to root_path
+    end
   end
 end
